3933ab0976f08597be2ba7704ea98c134cba1b46
Elias Araya Diaz <Elias@MacBook-Air-de-macbook.local>
2018-05-06 20:09:24 -0300

    Modificaciones en el loop de filto

--- program.py
+++ program.py
@@ -1,10 +1,38 @@
+import cv2
 import glob
 import os
 import json
+
+import numpy as np
 from bs4 import BeautifulSoup
 import requests
+src_path = "/Users/macbook/Desktop"
+def get_string(img_path):
 
-src_path = r"/Users/macbook/Desktop"
+    # Si no hay imagen en el desktop, termina el loop
+    if(img_path == "/Users/macbook/Desktop/Elias"):
+        print("No hay imagen disponible")
+        exit(1)
+
+    # Read image with opencv
+    img = cv2.imread(img_path)
+
+    # Convert to gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Write image after removed noise
+    cv2.imwrite(src_path + r"/removed_noise.png", img)
+
+    #  Apply threshold to get image with only black and white
+    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+    # Write the image after apply opencv to do some ...
+    #cv2.imwrite(src_path + r"/thres.png", img)
 
 
 def ocr_space_file(filename, overlay=False, api_key='af599d01d888957', language='eng'):
@@ -37,6 +65,11 @@ def search():
     list_of_files = glob.glob('/Users/macbook/Desktop/*')
 
     # Devuelve el path del último archivo del desktop
+    latest_file = max(list_of_files, key=os.path.getctime)
+    get_string(str(latest_file))
+
+    # Se consulta por la última imagen en blanco y negro
+    list_of_files = glob.glob('/Users/macbook/Desktop/*')
     latest_file = max(list_of_files, key=os.path.getctime)
 
     # Devuelve un string con el texto de la imagen
@@ -74,14 +107,14 @@ def search():
             else:
                 str_api[cont_aux - 1] += " " + i
             cont += 1
-
-    # Une q_string en una sola linea
+    cont = 0
+    # Une q_string en un solo string
     q_string = " ".join(query_str)
 
     # Pregunta
     query = q_string
     print(query)
-
+    print(str_api)
     # Si el array string no llega a tener 4 valores, arroja una excepcion
     try:
 
@@ -96,7 +129,7 @@ def search():
     search = query.replace(" ", "+")
 
     # Se carga el enlace con la pregunta y palabras claves
-    google_search = "https://www.google.cl/search?q=" + search + " " + key + " " + key2 + " " + key3
+    google_search = 'https://www.google.cl/search?q=' + search +" "+key+" "+key2+" "+key3
 
     # Se realiza el request al enlace, retorna informacion de la pagina
     r = requests.get(google_search)
@@ -112,13 +145,13 @@ def search():
 
         # Entrega una linea obviando los tag's intermedios
         value = i.get_text()
-
+        print (value)
         # Se contabilizan concurrencias de palabras claves en una linea
-        if key in value:
+        if key.lower() in value.lower():
             cont += 1
-        if key2 in value:
+        if key2.lower() in value.lower():
             cont2 += 1
-        if key3 in value:
+        if key3.lower() in value.lower():
             cont3 += 1
 
     # Suma total de ocurrencias
